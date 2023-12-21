2d57c86624e96e0bdf5f3a25be73b3ae9f0e2485
Brian Chen <enderchen004@outlook.com>
2023-12-21 11:48:24 +0000

    Add more features

--- README.md
+++ README.md
@@ -2,28 +2,53 @@
 
 A small Python script that helps CUED students (IA/IB) download and combine lab and lecture calendars, removing useless information and rearranging properties, and filtering lectures according to the students' will.
 
-## Running the script
-
-Clone the repo / download zip & unzip
+## Installation
 
 ```bash
 git clone https://github.com/differental/CUED_CalMerge.git
 cd CUED_CalMerge
 pip install -r requirements.txt
-python main.py
 ```
+
+## Running the script
+
+```
+usage: main.py [-h] [-l] [-b] [-c CONFIG]
+
+This tool helps you deal with CUED Calendars. Please refer to README.md for
+configuration guides.
+
+options:
+  -h, --help            show this help message and exit
+  -l, --skip-lecture    Skip Lectures (default: False)
+  -b, --skip-lab        Skip Lab (default: False)
+  -c CONFIG, --config CONFIG
+                        Use Configuration file (default: None)
+```
+
+Examples:
+
+- `python main.py -h`: Display the help message above
+- `python main.py -l`: Skip Lectures, and enter commandline configuration
+- `python main.py -c config.ini`: Use `config.ini` as configuration file and skip commandline configuration
+- `python main.py -l -c config.ini`: Use `config.ini` as configuration file, skip commandline configuration, and skip lectures
+- `python main.py -l -b`: Bro stop messing around please
 
 ## Expected output
 
-`lecture.ics`: Optimised and filtered lecture calendar file;
-
-if Lab Group Number is not set to 0 (skip):
-    `lab.ics`: Optimised and filtered lab calendar file;
-    `combined.ics`: Combination of lecture and lab calendars, optimised and filtered.
+- `lecture.ics`: Optimised and filtered lecture calendar file, if enabled;
+- `lab.ics`: Optimised and filtered lab calendar file, if enabled;
+- `combined.ics`: Combination of lecture and lab calendars, optimised and filtered, if both lecture and lab are enabled.
 
 To import the generated `ics` files to your own calendar, simply open them, or go to Google Calendar / Outlook Calendar / iCloud and import them.
 
-## Configuration
+## Configuration file
+
+You can either choose to use the configuration file with `-c /path/to/config` or use console configuration when `-c` is not specified.
+
+See `config.ini` for a sample configuration file including comments.
+
+## Console configuration
 
 | Option           | Description                                                                                                                                     | Default Value                                  |
 | ---------------- | ----------------------------------------------------------------------------------------------------------------------------------------------- | ---------------------------------------------- |
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 """
 main.py
 """
+import sys
+import argparse
 from datetime import datetime
 import math
 from icalendar import Calendar, Event
 import requests
 import pytz
 import recurring_ical_events
+from configparser import ConfigParser
 
 def handle_events(list_of_events, lec_clean: bool, lab_clean: bool, opt_mode: int):
     """
@@ -50,6 +53,43 @@ def handle_events(list_of_events, lec_clean: bool, lab_clean: bool, opt_mode: in
         cal.add_component(newevent)
     return cal
 
+parser = argparse.ArgumentParser(description="""
+This tool helps you deal with CUED Calendars.
+Please refer to README.md for configuration guides.""",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-l", "--skip-lecture", action="store_true", help="Skip Lectures")
+parser.add_argument("-b", "--skip-lab", action="store_true", help="Skip Lab")
+parser.add_argument("-c", "--config", help="Use Configuration file")
+args = parser.parse_args()
+config = vars(args)
+
+SKIP_LECTURE = bool(config['skip_lecture'])
+SKIP_LAB = bool(config['skip_lab'])
+config_path = str(config['config']) if config['config'] else None
+
+if SKIP_LECTURE and SKIP_LAB:
+    print("bro stop messing around please")
+    sys.exit(1)
+    
+skip_config = False
+
+if config_path:
+    config_object = ConfigParser()
+    config_object.read(config_path)
+
+    basicinfo = config_object["INFO"]
+    YR = int(basicinfo["academic_year"])
+    TERMID = "mich" if str(basicinfo["term"]) == "Michaelmas" else str(basicinfo["term"]).lower()
+    LAB_GROUP = int(basicinfo["lab_group"])
+    IA_OR_IB = 1 if str(basicinfo["year_group"]) == "IA" else 2
+
+    optioninfo = config_object["OPTION"]
+    LECTURE_CLEAN = str(optioninfo["clean_lecture"]) == "True"
+    LAB_CLEAN = str(optioninfo["clean_lab"]) == "True"
+    operating_mode = int(optioninfo["lecture_filter"])
+    
+    skip_config = True
+
 now = datetime.now(pytz.timezone("Europe/London"))
 recommend_year = now.year
 if now.month <= 6:
@@ -61,20 +101,40 @@ Use lecture.ics for lecture, lab.ics for lab, and combined.ics for both.
 Open the calendar file to import to your calendar.
 """)
 
-YR = int(
-    input("Academic Year [" + str(recommend_year) + "]: ").strip() or recommend_year)
+if not skip_config:
 
-ia_or_ib = int(input("\n1. IA\n2. IB\nChoose Year Group: "))
+    YR = int(
+        input("Academic Year [" + str(recommend_year) + "]: ").strip() or recommend_year)
 
-term = int(input("\n1. Michaelmas\n2. Lent\n3. Easter\nChoose Term: "))
-TERMID = "mich" if term == 1 else "lent" if term == 2 else "easter"
+    IA_OR_IB = int(input("\n1. IA\n2. IB\nChoose Year Group: "))
 
-LAB_GROUP = int(input("Lab Group Number [Skip Lab]: ").strip() or 0)
+    term = int(input("\n1. Michaelmas\n2. Lent\n3. Easter\nChoose Term: "))
+    TERMID = "mich" if term == 1 else "lent" if term == 2 else "easter"
+
+    LAB_GROUP = int(input("Lab Group Number [Skip Lab]: ").strip() or 0) if not SKIP_LAB else 0
+    SKIP_LAB = not bool(LAB_GROUP)
+
+    if SKIP_LECTURE and SKIP_LAB:
+        print("bro stop messing around please")
+        sys.exit(1)
+
+    LECTURE_CLEAN = (not bool(int(input("""
+1. Clean lecture titles and put locations in a seperate line
+2. Do Nothing
+Choose Mode [1]: """).strip() or 1) - 1)) if not SKIP_LECTURE else False
+
+    LAB_CLEAN = (not bool(int(input("""
+1. Clean lab titles and "Cambridge, United Kingdom" in lab locations
+2. Do Nothing
+Choose Mode [1]: """).strip() or 1) - 1)) if not SKIP_LAB else False
+    
+    operating_mode = int(input(
+    "\n1. Bare Minimum\n2. Essential\n3. All\nChoose Mode [2]: ").strip() or 2)
 
 LAB_URL = '/'.join((
     "http://teach-cals.eng.cam.ac.uk/cued-labs",
     str(YR),
-    "ia" if ia_or_ib == 1 else "ib",
+    "ia" if IA_OR_IB == 1 else "ib",
     TERMID,
     str(math.ceil(LAB_GROUP/3)*3-2) + "-" + str(math.ceil(LAB_GROUP/3)*3) + ".ics"))
 
@@ -82,48 +142,32 @@ LECTURE_URL = '&'.join((
     "https://td.eng.cam.ac.uk/tod/public/view_ical.php?yearval=" +
     str(YR) + "_" + str((YR+1) % 100),
     "term=" + TERMID[0].upper(),
-    "course=" + ("IA" if ia_or_ib == 1 else "IB")))
+    "course=" + ("IA" if IA_OR_IB == 1 else "IB")))
 
-LECTURE_CLEAN = bool(int(input("""
-1. Clean lecture titles and put locations in a seperate line
-2. Do Nothing
-Choose Mode [1]: """).strip() or 1))
-
-LAB_CLEAN = bool(int(input("""
-1. Clean lab titles and "Cambridge, United Kingdom" in lab locations
-2. Do Nothing
-Choose Mode [1]: """).strip() or 1))
-
-operating_mode = int(input(
-    "\n1. Bare Minimum\n2. Essential\n3. All\nChoose Mode [2]: ").strip() or 2)
-
-if LAB_GROUP:
+if not SKIP_LAB:
     labr = requests.get(LAB_URL, allow_redirects=True, timeout=10)
     open('lab.ics', 'wb').write(labr.content)
-lecturer = requests.get(LECTURE_URL, allow_redirects=True, timeout=10)
-open('lecture.ics', 'wb').write(lecturer.content)
-
-if LAB_GROUP:
     with open('lab.ics', 'rb') as labe:
         lab_cal = Calendar.from_ical(labe.read())
-
-with open('lecture.ics', 'rb') as lecturee:
-    lecture_cal = Calendar.from_ical(lecturee.read())
-
-lecture_events = recurring_ical_events.of(
-    lecture_cal).between("19700101", "20991231")
-lecture_new = handle_events(lecture_events, LECTURE_CLEAN, LAB_CLEAN, operating_mode)
-with open('lecture.ics', 'wb') as f:
-    f.write(lecture_new.to_ical())
-
-if LAB_GROUP:
     lab_events = recurring_ical_events.of(
         lab_cal).between("19700101", "20991231")
-    combined_events = lecture_events + lab_events
     lab_new = handle_events(lab_events, LECTURE_CLEAN, LAB_CLEAN, operating_mode)
-    combined_new = handle_events(combined_events, LECTURE_CLEAN, LAB_CLEAN, operating_mode)
     with open('lab.ics', 'wb') as f:
         f.write(lab_new.to_ical())
+
+if not SKIP_LECTURE:
+    lecturer = requests.get(LECTURE_URL, allow_redirects=True, timeout=10)
+    open('lecture.ics', 'wb').write(lecturer.content)
+    with open('lecture.ics', 'rb') as lecturee:
+        lecture_cal = Calendar.from_ical(lecturee.read())
+    lecture_events = recurring_ical_events.of(
+        lecture_cal).between("19700101", "20991231")
+    lecture_new = handle_events(lecture_events, LECTURE_CLEAN, LAB_CLEAN, operating_mode)
+    with open('lecture.ics', 'wb') as f:
+        f.write(lecture_new.to_ical())
+
+if not (SKIP_LAB or SKIP_LECTURE):
+    combined_events = lecture_events + lab_events
+    combined_new = handle_events(combined_events, LECTURE_CLEAN, LAB_CLEAN, operating_mode)
     with open('combined.ics', 'wb') as f:
         f.write(combined_new.to_ical())
-
